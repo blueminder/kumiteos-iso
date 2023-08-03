a27e82e243e4d4c6a31b039dcbcec5e8ce6908a0
Enrique Santos <me@eos.fyi>
2023-08-03 03:54:33 +0300

    remove aui install step; preinstall pkg instead

--- kumiteos/airootfs/etc/skel/.scripts/write-persistent-usb.sh
+++ kumiteos/airootfs/etc/skel/.scripts/write-persistent-usb.sh
@@ -7,12 +7,6 @@ echo "Select Device to Write KumiteOS (USB Persistent) Image."
 
 select v in `lsblk --nodeps -o NAME -dn`
 do
-    if ! command -v aui-mkusb &> /dev/null
-    then
-        sudo reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
-        sudo pacman -U --noconfirm /root/packages/archuseriso-0.7.9-1-any.pkg.tar.zst
-    fi
-
 	sudo umount --all-targets /dev/${v}
 	sudo aui-mkusb kumiteos-latest.iso /dev/${v}
     exit 0
